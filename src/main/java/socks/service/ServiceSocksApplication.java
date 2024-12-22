2b092947d17ae96dfb970b226ff7bd31a292468a
TemaGreen1997 <arhamidullin@gmail.com>
2024-12-22 04:27:18 +0400

    update swagger description

--- src/main/java/socks/service/ServiceSocksApplication.java
+++ src/main/java/socks/service/ServiceSocksApplication.java
@@ -1,9 +1,16 @@
 package socks.service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Service Socks ",
+				version = "1.0",
+				description = "This API was created as part of a test task"))
 public class ServiceSocksApplication {
 
 	public static void main(String[] args) {
--- src/main/java/socks/service/model/Socks.java
+++ src/main/java/socks/service/model/Socks.java
@@ -1,22 +1,31 @@
 package socks.service.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Size;
+
 public class Socks {
 
+    @Schema(example = "1")
     private Long id;
 
     /**
      * The RGB color for socks in hexadecimal notation, which is written as string
      */
+    @Schema(description = "The color of the sock is represented as a string in hexadecimal format, which is then converted into RGB", example = "#FFFFFF")
     private String color;
 
     /**
      * The percentage of cotton written as an integer from 0 to 100
      */
+    @Schema(description = "The percentage of cotton in socks", example = "50")
+    @Size(min = 0, max = 100)
     private Integer cotton;
 
     /**
      * Amount of socks available in storage
      */
+    @Schema(description = "The amount of socks in stock", example = "25")
+    @Size(min = 0)
     private Long amount;
 
     public Long getId() {
