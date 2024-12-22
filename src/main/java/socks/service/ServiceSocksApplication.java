1d3c6f308a6afd77da90e89ee2aed3e0f29d1cf2
TemaGreen1997 <arhamidullin@gmail.com>
2024-12-22 04:02:37 +0400

    init

---
+++ src/main/java/socks/service/ServiceSocksApplication.java
@@ -0,0 +1,13 @@
+package socks.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceSocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceSocksApplication.class, args);
+	}
+
+}
